09350d848669d1cb63a1259ac61083e11ce09c79
Allana Mendes <allana.mendes@INSOLE.LOCAL>
2022-03-03 13:53:21 -0300

    :lipstick: início da estilização da página de login

--- src/pages/LogIn/index.jsx
+++ src/pages/LogIn/index.jsx
@@ -1,5 +1,7 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import "./styles.css";
 import React, { useState } from 'react';
+import ImgLogo from '../../assets/logo.png';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { Link } from 'react-router-dom';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import { auth } from '../../config/firebase-config';
@@ -28,30 +30,35 @@ function LogIn() {
   };
 
   return (
-    <div className="login-user">
+    <div className="login-body">
       {userCurrentlyLogged ?
         <Redirect to="/" />
         :
         <>
-          <h3>Login de usuário</h3>
-          <input
-            value={loginEmail}
-            type="email"
-            placeholder="E-mail"
-            onChange={(event) => {
-              setLoginEmail(event.target.value);
-            }}
-          />
-          <input
-            value={loginPassword}
-            type="password"
-            placeholder="Senha"
-            onChange={(event) => {
-              setLoginPassword(event.target.value);
-            }}
-          />
-          <button onClick={login}>Enviar</button>
-          <Link to="/sign-up">Ainda não é cadastrado? Cadastre-se.</Link>
+          <img className="animal-crossing-logo" src={ImgLogo} alt="logotipo animal crossing" />
+          <section className="login-container">
+            <h3 className="form-title">Welcome back!</h3>
+            <input
+              className="form-input"
+              value={loginEmail}
+              type="email"
+              placeholder="E-mail"
+              onChange={(event) => {
+                setLoginEmail(event.target.value);
+              }}
+            />
+            <input
+              className="form-input"
+              value={loginPassword}
+              type="password"
+              placeholder="Senha"
+              onChange={(event) => {
+                setLoginPassword(event.target.value);
+              }}
+            />
+            <button className="submit-btn" onClick={login}>Login</button>
+            <Link className="redirect-link" to="/sign-up">Ainda não é cadastrado? Cadastre-se.</Link>
+          </section>
         </>
       }
     </div>
